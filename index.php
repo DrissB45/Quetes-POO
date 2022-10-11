874a7a19c54b3d43190554a25d1d83ed9a44d5ec
DRISS BOUMEDIENE <driss308@hotmail.fr>
2022-10-11 22:33:19 +0200

    Quête PDO

--- index.php
+++ index.php
@@ -1,20 +1,55 @@
 <?php
 
-//Appel des fichiers contenant les classes
+require 'connec.php';
 
-require_once 'vehicle.php';
-require_once 'truck.php';
+$pdo = new \PDO(DSN, USER, PASS);
 
-//Instanciation d'un nouveau camion
+$query = 'SELECT * FROM friend';
+$statement = $pdo->query($query);
+$friendsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$scania = new Truck(500, 'red', 3, 'fuel');
+$query = 'INSERT INTO friend (firstname, lastname) VALUES (:firstname, :lastname)';
+$statement = $pdo->prepare($query);
 
-var_dump($scania);
+$statement->bindValue(':firstname', $_POST['firstname'], \PDO::PARAM_STR);
+$statement->bindValue(':lastname', $_POST['lastname'], \PDO::PARAM_STR);
+$statement->execute();
 
-//Test des fonctions de mon camion
+$newFriend = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo $scania->forward() . PHP_EOL;
+?>
 
-echo $scania->brake() . PHP_EOL;
+<!DOCTYPE html>
+<html lang="en">
 
-echo $scania->loading() . PHP_EOL;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="form.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($friendsArray as $friend) { ?>
+            <li><?= $friend['firstname'] . ' ' . $friend['lastname'] ?></li>
+        <?php } ?>
+    </ul>
+
+    <h1>Formulaire de Friend</h1>
+    <form action="" method="post">
+        <div>
+            <label for="firstname">First name :</label>
+            <input type="text" id="firstname" name="firstname">
+        </div>
+        <div>
+            <label for="lasttname">Last name :</label>
+            <input type="text" id="lastname" name="lastname">
+        </div>
+        <button>Submit</button>
+    </form>
+
+</body>
+
+</html>
